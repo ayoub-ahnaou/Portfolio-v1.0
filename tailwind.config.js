114cc0a413d58fea71f9542319409cb8cbe4531c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-19 20:54:46 +0100

    Block the predefined class of container and define the color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  blocklist: ["container"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#03AA64",
+        secondTextColor: "#767676",
+        primaryTextColor: "#000000",
+        lightBg: "#F5F5F5",
+        darkBg: "#242020",
+      }
+    },
   },
   plugins: [],
 }
