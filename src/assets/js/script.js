b9c5ccecdc8002932eff57e960f4fa651f683b62
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-22 21:25:38 +0100

    Fill the three box with social media information in skills section

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -26,3 +26,29 @@ skills.map((skill) => {
 
     skillsContainer.appendChild(skillElement);
 });
+
+
+import socials from "../data/social.json" with { type: "json" };
+const socialContainer = document.getElementById("social-container");
+socials.map((social) => {
+    socialContainer.innerHTML += `
+            <div
+              class="bg-lightBg shadow-md rounded-md w-1/3 max-md:w-full p-4 flex flex-col max-md:flex-row max-md:items-center justify-between"
+            >
+                <div class="flex gap-4 flex-col max-md:flex-row max-md:items-center">
+                    <img
+                        src=${social.img_url}
+                        alt=${social.name}
+                        class="img-effect size-12 max-md:size-16"
+                    />
+                    <div class="flex flex-col">
+                        <p class="text-sm text-primaryTextColor">${social.name}</p>
+                        <p class="text-secondTextColor text-base font-mono">${social.account}</p>
+                    </div>
+                </div>
+                <div class="">
+                    <button class="primary-btn-small">FOLLOW</button>
+                </div>
+            </div>
+    `;
+});
